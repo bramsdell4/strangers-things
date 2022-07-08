df6782a92167544db26d3f1524588fde6b04e08b
Basil Ramsdell <bramsdell72@gmail.com>
2022-07-07 21:49:27 -0600

    fancy nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
 import React , {useState} from "react";
-import {Route} from "react-router-dom";
+import { NavLink, Route} from "react-router-dom";
 
 const App = () => {
     const [posts, setPosts] = useState([])
     const [token, setToken] = useState('')
+    const [user, setUser] = useState(false)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [featuredPost, setFeaturedPost] = useState('')
+
 
 
     return <main>
+        <nav>
+        <NavLink exact to="/" className="navlink" activeClassName="active">
+            Home
+            </NavLink>
+        <NavLink to="/posts" className="navlink" activeClassName="active">
+            Posts
+            </NavLink>
+        <NavLink to="/login" className="navlink" activeClassName="active">
+            Login
+            </NavLink>
+        <NavLink to="/register" className="navlink" activeClassName="active">
+            Register
+            </NavLink>
+        </nav>
         <Route exact path="/"><div>hello</div></Route>
         <Route path="/posts"><div>posts</div></Route>
         <Route path="/login"><div>Login</div></Route>
---
+++ src/utility/api.js
@@ -0,0 +1,38 @@
+const strangerThingsAPI_URL = 'https://strangers-things.herokuapp.com/api/2204-FTB-MT-WEB-PT'
+
+export const apiCall = async (url, method='GET', token, body) => {
+
+    let data = false;
+    try {
+      const response = await fetch(
+        strangerThingsAPI_URL + url, 
+        setToken(getFetchOptions(method, body), token)
+      );
+      data = await response.json();
+  
+      if (data.error) {
+        throw data.error;
+      }
+    } catch(e) {
+      console.error(e);
+    }
+  
+    return data;
+  }
+  
+  
+  const getFetchOptions = (method, body) => {
+    return {
+      method: method.toUpperCase(),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    }
+  }
+  
+  const setToken = (body, token) => {
+    return token ? 
+      Object.assign(body, {'Authorization': `Bearer ${token}` }) :
+      body;
+  }
